520407ce915ff81ffbb8cd6528b525bbf3f49350
mgajek <gajekmagdalena2@gmail.com>
2024-03-13 07:37:27 +0100

    some refactor

--- practice/vue-app/src/components/Form.vue
+++ practice/vue-app/src/components/Form.vue
@@ -2,15 +2,25 @@
   <form>
     <div>
       <label for="firstName">First Name:</label>
-      <input v-model="firstName" type="text" id="firstName" name="firstName" />
+      <input
+        v-model="user.firstName"
+        type="text"
+        id="firstName"
+        name="firstName"
+      />
     </div>
     <div>
       <label for="lastName">Last Name:</label>
-      <input v-model="lastName" type="text" id="lastName" name="lastName" />
+      <input
+        v-model="user.lastName"
+        type="text"
+        id="lastName"
+        name="lastName"
+      />
     </div>
     <div>
       <label for="age">Age:</label>
-      <input v-model="age" id="age" name="age" />
+      <input v-model="user.age" id="age" name="age" />
     </div>
     <span v-for="error in errors" :key="error" class="error"> {{ error }}</span>
     <span
@@ -29,14 +39,21 @@ import { formValidator } from '../helpers/validator';
 export default defineComponent({
   name: 'Form',
   setup() {
-    const firstName = ref('');
-    const lastName = ref('');
-    const age = ref(0);
+    const user = ref({
+      firstName: '',
+      lastName: '',
+      age: 0,
+    } as User);
+
     const errors = ref([] as string[]);
     const message = ref('Fill all the fields to submit the form.');
 
     function handleSubmit() {
-      errors.value = formValidator(firstName.value, lastName.value, age.value);
+      errors.value = formValidator(
+        user.value.firstName,
+        user.value.lastName,
+        user.value.age
+      );
       if (!!errors.value.length) {
         message.value = 'Form has errors!';
       } else {
@@ -45,9 +62,7 @@ export default defineComponent({
     }
 
     return {
-      firstName,
-      lastName,
-      age,
+      user,
       errors,
       message,
       handleSubmit,
